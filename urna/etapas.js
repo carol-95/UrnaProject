84726b03f39e3393e1b219e20ce2bb4a77509bc9
Maria Carolina <mcarolina1950@hotmail.com>
2022-01-05 19:09:17 -0300

    Primeiro commit

---
+++ urna/etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+{
+  titulo: 'VEREADOR',
+  numeros: 5,
+  candidatos: [
+    {
+      numero: 38111,
+      nome: 'Fulano de Tal',
+      partido: 'ABC',
+      fotos: [
+        {url:'1.jpg', legenda: 'vereador'}
+
+]
+    },
+    {
+      numero:77222,
+      nome: 'Beltrano da Silva',
+      partido: 'DEFG',
+      fotos:[
+        {url:'2.jpg', legenda: 'vereador'}
+      ]
+    },
+  ]
+},
+{
+  titulo: 'PREFEITO',
+  numeros: 2,
+  candidatos: [
+    {
+      numero:99,
+      nome: 'ciclano',
+      partido: 'ABC',
+      vice: 'Cic',
+      fotos: [
+        {url:'3.jpg', legenda: 'prefeito'},
+        {url:'4.jpg', legenda: 'Vice-Prefeito', small: true}
+      ]
+    },
+    {
+      numero: 84,
+      nome: 'Zulano',
+      Partido: 'QWERTY',
+      vice: 'Zul',
+      fotos: [
+        {url:'5.jpg', legenda: 'Prefeito'},
+        {url:'6.jpg', legenda: 'Vice-Prefeito', small: true}
+      ]
+    },
+  ]
+}
+];
+      
